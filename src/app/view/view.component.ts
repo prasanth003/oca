7cdb12fe519d97d3efdeb742344391076d6d362c
Prasanth Sekar <prasanth.sekar@sisainfosec.com>
2023-03-26 01:10:01 +0530

    sidebar theme updated

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,5 +1,8 @@
 import { Component, HostListener, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Store } from '@ngrx/store';
+import { iState, iTheme } from '../shared/interface/state.interface';
+import { Defaults } from '../shared/configuration/defaults.config';
 
 @Component({
   selector: 'app-view',
@@ -12,14 +15,22 @@ export class ViewComponent implements AfterViewInit {
   public availableHeight: number = 0;
   public sidebarWidth: number = 70;
   public navbarHeight: number = 70;
+  public theme: string = Defaults.Theme;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private cdRef: ChangeDetectorRef
+    private cdRef: ChangeDetectorRef,
+    private store: Store<iState> 
   ) {
+    
     this.breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
       this.isHandset = result.matches;
     });
+
+    this.store.select(state => state.theme).subscribe({
+      next: (theme: iTheme) => this.theme = theme.name
+    });
+
   }
 
   /* Listing on window resize */
